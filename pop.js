f02f7485720a5b4bbd8cd33b60a8574e95d2aeba
Prakash9019 <plsprakash2003@gmail.com>
2024-05-11 14:43:28 +0530

    added

--- pop.js
+++ pop.js
@@ -3,10 +3,9 @@ const title = document.querySelector('.selectedColor');
 // const colorValue = document.querySelector('.colorValue');
 
 btn.addEventListener('click', async () => {
-    chrome.storage.sync.get('color', ({ color }) => {
-        console.log('color: ', color);
-    });
+    // chrome.storage.sync.get('color', ({ color }) => {
+    //     console.log('color: ', color);
+    // });
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    console.log(tab);
     title.innerHTML = tab.title;
 });
